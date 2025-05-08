9b7d8f21267e364f88aa956edccb034a5ac2e8bd
rajkagrawal <rajaanova@gmail.com>
2025-05-08 13:15:09 +0530

    Update main.go

--- main.go
+++ main.go
@@ -8,8 +8,25 @@ import (
 
 func main() {
 	fmt.Println("in main")
+	WriteFile()
 	ReadFile()
 	fmt.Println(" going out of main")
+}
+
+func WriteFile() {
+	fmt.Println("in write file")
+	f, err := os.OpenFile("test.txt", os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer f.Close()
+	_, err = f.WriteString("hello world")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println("file written successfully")
 }
 
 func ReadFile() {
